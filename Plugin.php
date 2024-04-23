82fdc5870ae6faadeead1b4c9725be181dadf065
Tuncay Alarcin <tuncayalarcin@gmail.com>
2024-04-23 16:13:30 +0200

    Update test class

--- Plugin.php
+++ Plugin.php
@@ -5,6 +5,7 @@ namespace Tuncay\PsalmWpTaint;
 use SimpleXMLElement;
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
+use Tuncay\PsalmWpTaint\src\CustomTaintSourcesAdder;
 use Tuncay\PsalmWpTaint\src\TestPlugin;
 
 class Plugin implements PluginEntryPointInterface
@@ -18,7 +19,9 @@ class Plugin implements PluginEntryPointInterface
     public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
     {
         require_once __DIR__ . '/src/TestPlugin.php';
+        require_once __DIR__ . '/src/CustomTaintSourcesAdder.php';
         $registration->registerHooksFromClass(TestPlugin::class);
+        $registration->registerHooksFromClass(CustomTaintSourcesAdder::class);
 
         foreach ($this->getStubFiles() as $file) {
             print_r("Current stub file" . $file . "\n");
---
+++ src/CustomTaintSourcesAdder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tuncay\PsalmWpTaint\src;
+
+use PhpParser\Node\Expr\Variable;
+use Psalm\CodeLocation;
+use Psalm\Plugin\EventHandler\AfterExpressionAnalysisInterface;
+use Psalm\Plugin\EventHandler\Event\AfterExpressionAnalysisEvent;
+use Psalm\Type\TaintKind;
+use Psalm\Type\TaintKindGroup;
+
+class CustomTaintSourcesAdder implements AfterExpressionAnalysisInterface
+{
+
+    public static function afterExpressionAnalysis(AfterExpressionAnalysisEvent $event): ?bool
+    {
+        $expr = $event->getExpr();
+        $stmt_source = $event->getStatementsSource();
+        $codebase = $event->getCodebase();
+
+        if ($expr instanceof Variable) {
+            print_r('Found expression ' . $expr->name . PHP_EOL);
+
+            if ($expr->name == '_POST') {
+                $expr_type = $stmt_source->getNodeTypeProvider()->getType($expr);
+
+                // should be a globally unique id
+                // you can use its line number/start offset
+                $expr_identifier = '$bad_data'
+                    . '-' . $stmt_source->getFileName()
+                    . ':' . $expr->getAttribute('startFilePos');
+
+                if ($expr_type) {
+                    $codebase->addTaintSource(
+                        $expr_type,
+                        $expr_identifier,
+                        TaintKindGroup::ALL_INPUT,
+                        new CodeLocation($stmt_source, $expr)
+                    );
+                }
+            }
+        }
+
+        return null;
+    }
+}
