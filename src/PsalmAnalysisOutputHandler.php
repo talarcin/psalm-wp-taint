fb877dae5580c3948f02c121f03b32f70c11a6b1
Tuncay Alarcin <tuncayalarcin@gmail.com>
2024-06-19 10:40:42 +0200

    Update analysis result output with more information

--- src/PsalmAnalysisOutputHandler.php
+++ src/PsalmAnalysisOutputHandler.php
@@ -11,12 +11,23 @@ class PsalmAnalysisOutputHandler
 
     public function handle(PsalmOutputParser $outputParser, array $outputs): array
     {
-        $results = array();
+        $results = array(
+            "total" => count($outputs),
+            "total_tainted_plugins" => 0,
+            "total_no_taint" => 0,
+            "results" => []
+        );
 
         foreach ($outputs as $pluginSlug => $output) {
             if (is_array($output)) {
                 $errors = $outputParser->parsePsalmOutput($output);
-                $results[$pluginSlug] = $errors;
+                if (!$errors) {
+                    $results["total_no_taint"]++;
+                } else {
+                    $results["total_tainted_plugins"]++;
+                }
+
+                $results["results"][$pluginSlug] = $errors;
             }
         }
 
