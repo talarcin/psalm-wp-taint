0fd687042de12e904d75fdf0fc30b77aed6eee73
Tuncay Alarcin <tuncayalarcin@gmail.com>
2024-08-02 11:41:29 +0200

    Refactor Util.php

--- src/Util.php
+++ src/Util.php
@@ -7,57 +7,49 @@ use RecursiveIteratorIterator;
 use RecursiveRegexIterator;
 use RegexIterator;
 
-class Util
-{
-    public static function changePsalmProjectDir(string $pluginDirPath, string $psalmXMLPath): void
-    {
-        $psalm_xml = simplexml_load_file($psalmXMLPath);
+class Util {
+	public static function changePsalmProjectDir( string $pluginDirPath, string $psalmXMLPath ): void {
+		$psalm_xml = simplexml_load_file( $psalmXMLPath );
 
-        if (!$psalm_xml->projectFiles->directory) {
-            print_r("Configuring psalm.xml with correct project directory ...\n");
+		if ( ! $psalm_xml->projectFiles->directory ) {
+			print_r( "Configuring psalm.xml with correct project directory ...\n" );
+			$psalm_xml->projectFiles->addChild( "directory" );
+			$psalm_xml->projectFiles->directory->addAttribute( "name", $pluginDirPath );
+		} else if ( ! $psalm_xml->projectFiles->directory["name"] ) {
+			print_r( "Configuring psalm.xml with correct project directory ...\n" );
+			$psalm_xml->projectFiles->directory->addAttribute( "name", $pluginDirPath );
+		} else {
+			print_r( "Configuring psalm.xml with correct project directory ...\n" );
+			$psalm_xml->projectFiles->directory["name"] = $pluginDirPath;
+		}
 
-            $psalm_xml->projectFiles->addChild("directory");
-            $psalm_xml->projectFiles->directory->addAttribute("name", $pluginDirPath);
-        }
+		$psalm_xml->asXML( $psalmXMLPath );
+		print_r( "Configuration of psalm.xml done!\n" );
+	}
 
-        if (!$psalm_xml->projectFiles->directory["name"]) {
-            print_r("Configuring psalm.xml with correct project directory ...\n");
-            $psalm_xml->projectFiles->directory->addAttribute("name", $pluginDirPath);
-        } else {
-            print_r("Configuring psalm.xml with correct project directory ...\n");
-            $psalm_xml->projectFiles->directory["name"] = $pluginDirPath;
-        }
-
-        $psalm_xml->asXML("./psalm.xml");
-        print_r("Configuration of psalm.xml done!\n");
-    }
-
-    public static function scanDirForPHPFiles(string $directory): array
-    {
-        $directoryIterator = new RecursiveDirectoryIterator($directory);
-        $iterator = new RecursiveIteratorIterator($directoryIterator);
-        $regex = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+	public static function scanDirForPHPFiles( string $directory ): array {
+		$directoryIterator = new RecursiveDirectoryIterator( $directory );
+		$iterator          = new RecursiveIteratorIterator( $directoryIterator );
+		$regex             = new RegexIterator( $iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH );
 
 		$files = [];
 
-		foreach (iterator_to_array($regex) as $key => $file) {
+		foreach ( iterator_to_array( $regex ) as $key => $file ) {
 			$files[] = $key;
 		}
 
-        return $files;
-    }
+		return $files;
+	}
 
-    public static function getDirsIn(string $directory): array|false
-    {
-        if (str_ends_with($directory, "/")) {
-            return glob($directory . '*', GLOB_ONLYDIR);
-        }
+	public static function getDirsIn( string $directory ): array|false {
+		if ( str_ends_with( $directory, "/" ) ) {
+			return glob( $directory . '*', GLOB_ONLYDIR );
+		}
 
-        return glob("$directory/*", GLOB_ONLYDIR);
-    }
+		return glob( "$directory/*", GLOB_ONLYDIR );
+	}
 
-    public static function removeAnsiCodes(string $line): string
-    {
-        return preg_replace('#\\x1b[[][0-9]+(;[0-9]*)[A-Za-z]#', '', $line);
-    }
+	public static function removeAnsiCodes( string $line ): string {
+		return preg_replace( '#\\x1b[[][0-9]+(;[0-9]*)[A-Za-z]#', '', $line );
+	}
 }
--- tests/unit/UtilTest.php
+++ tests/unit/UtilTest.php
@@ -6,20 +6,62 @@ use PHPUnit\Framework\TestCase;
 use Tuncay\PsalmWpTaint\src\Util;
 
 class UtilTest extends TestCase {
-	const string TEST_DIR = "./tests/res/psalm/plugins/";
+	const string TEST_DIR_WITH_FSLASH = "./tests/res/psalm/plugins/";
+	const string TEST_DIR_WITHOUT_FSLASH = "./tests/res/psalm/plugins";
 	const string TEST_DIR2 = "./tests/res/psalm/files";
 
-	public function testGetDirsIn() {
+
+	protected function tearDown(): void {
+		$this->rewindXML();
+	}
+
+	public function testGetDirsInIfBranch() : void {
 		$expected = [ "./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record" ];
-		$actual   = Util::getDirsIn( self::TEST_DIR );
+		$actual   = Util::getDirsIn( self::TEST_DIR_WITH_FSLASH );
+
+		$this->assertEquals( $expected, $actual );
+	}
+	public function testGetDirsInElseBranch(): void {
+		$expected = [ "./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record" ];
+		$actual   = Util::getDirsIn( self::TEST_DIR_WITHOUT_FSLASH );
 
 		$this->assertEquals( $expected, $actual );
 	}
 
-	public function testScanDirForFiles() {
+	public function testScanDirForFiles(): void {
 		$expected = ["./tests/res/psalm/files/test-file.php", "./tests/res/psalm/files/test-file-2.php", "./tests/res/psalm/files/test-file-3.php"];
 		$actual = Util::scanDirForPHPFiles(self::TEST_DIR2);
 
 		$this->assertEquals( $expected, $actual );
+	}
+
+	public function testChangePsalmProjectDirWithoutDirectoryChild(): void {
+		$expected = "./tests/res/psalm/plugins/audio-record";
+		Util::changePsalmProjectDir("./tests/res/psalm/plugins/audio-record", "./tests/res/psalm/psalm.xml");
+		$actual = simplexml_load_file("./tests/res/psalm/psalm.xml")->projectFiles->directory["name"];
+
+		$this->assertEquals( $expected, $actual );
+	}
+	public function testChangePsalmProjectDirWithoutNameAttribute():void {
+		$expected = "./tests/res/psalm/plugins/audio-record";
+
+		$xml = simplexml_load_file("./tests/res/psalm/psalm.xml");
+
+		$xml->projectFiles->addChild("directory");
+
+		$xml->asXML("./tests/res/psalm/psalm.xml");
+
+		Util::changePsalmProjectDir("./tests/res/psalm/plugins/audio-record", "./tests/res/psalm/psalm.xml");
+		$actual = simplexml_load_file("./tests/res/psalm/psalm.xml")->projectFiles->directory["name"];
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+
+	private function rewindXML(): void {
+		$xml = simplexml_load_file( "./tests/res/psalm/psalm.xml" );
+
+		$xml->projectFiles = null;
+		$xml->asXML("./tests/res/psalm/psalm.xml");
 	}
 }
