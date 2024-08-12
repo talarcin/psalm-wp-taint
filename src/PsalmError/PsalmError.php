7937b15eb76e45c298c6265d630de253d0dc21c1
Tuncay Alarcin <tuncayalarcin@gmail.com>
2024-08-12 12:33:30 +0200

    Add PHPDoc

--- src/PsalmError/PsalmError.php
+++ src/PsalmError/PsalmError.php
@@ -1,26 +1,33 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace Tuncay\PsalmWpTaint\src\PsalmError;
 
-class PsalmError
-{
-  public string $errorType;
-  public string $errorPath;
-  public array $errorMessage;
+/**
+ * @author Tuncay Alarcin
+ */
+class PsalmError {
+	public string $errorType;
+	public string $errorPath;
+	public array $errorMessage;
 
-  public function __construct(string $type = "", string $path = "")
-  {
-    $this->errorType    = $type;
-    $this->errorPath    = $path;
-    $this->errorMessage = [];
-  }
+	public function __construct( string $type = "", string $path = "" ) {
+		$this->errorType    = $type;
+		$this->errorPath    = $path;
+		$this->errorMessage = [];
+	}
 
-  public function equals(PsalmError $other): bool
-  {
-    return $other->errorType == $this->errorType
-      && $other->errorPath == $this->errorPath
-      && $other->errorMessage == $this->errorMessage;
-  }
+	/**
+	 * Compares if given PsalmError has same field values as current.
+	 *
+	 * @param PsalmError $other
+	 *
+	 * @return bool
+	 */
+	public function equals( PsalmError $other ): bool {
+		return $other->errorType == $this->errorType
+		       && $other->errorPath == $this->errorPath
+		       && $other->errorMessage == $this->errorMessage;
+	}
 }
