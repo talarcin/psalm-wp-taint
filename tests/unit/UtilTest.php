b73a67e104e16b442880c267feee9d6c1204a4c7
Tuncay Alarcin <tuncayalarcin@gmail.com>
2024-08-20 10:42:14 +0200

    Fix wrong const declaration

--- tests/unit/UtilTest.php
+++ tests/unit/UtilTest.php
@@ -5,44 +5,51 @@ namespace Tuncay\PsalmWpTaint\tests\unit;
 use PHPUnit\Framework\TestCase;
 use Tuncay\PsalmWpTaint\src\Util;
 
-class UtilTest extends TestCase {
-	const string TEST_DIR_WITH_FSLASH = "./tests/res/psalm/plugins/";
-	const string TEST_DIR_WITHOUT_FSLASH = "./tests/res/psalm/plugins";
-	const string TEST_DIR2 = "./tests/res/psalm/files";
+class UtilTest extends TestCase
+{
+	const TEST_DIR_WITH_FSLASH = "./tests/res/psalm/plugins/";
+	const TEST_DIR_WITHOUT_FSLASH = "./tests/res/psalm/plugins";
+	const TEST_DIR2 = "./tests/res/psalm/files";
 
 
-	protected function tearDown(): void {
+	protected function tearDown(): void
+	{
 		$this->rewindXML();
 	}
 
-	public function testGetDirsInIfBranch() : void {
-		$expected = [ "./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record" ];
-		$actual   = Util::getDirsIn( self::TEST_DIR_WITH_FSLASH );
+	public function testGetDirsInIfBranch(): void
+	{
+		$expected = ["./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record"];
+		$actual   = Util::getDirsIn(self::TEST_DIR_WITH_FSLASH);
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals($expected, $actual);
 	}
-	public function testGetDirsInElseBranch(): void {
-		$expected = [ "./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record" ];
-		$actual   = Util::getDirsIn( self::TEST_DIR_WITHOUT_FSLASH );
+	public function testGetDirsInElseBranch(): void
+	{
+		$expected = ["./tests/res/psalm/plugins/adiaha-hotel", "./tests/res/psalm/plugins/audio-record"];
+		$actual   = Util::getDirsIn(self::TEST_DIR_WITHOUT_FSLASH);
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals($expected, $actual);
 	}
 
-	public function testScanDirForFiles(): void {
+	public function testScanDirForFiles(): void
+	{
 		$expected = ["./tests/res/psalm/files/test-file.php", "./tests/res/psalm/files/test-file-2.php", "./tests/res/psalm/files/test-file-3.php"];
 		$actual = Util::scanDirForPHPFiles(self::TEST_DIR2);
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals($expected, $actual);
 	}
 
-	public function testChangePsalmProjectDirWithoutDirectoryChild(): void {
+	public function testChangePsalmProjectDirWithoutDirectoryChild(): void
+	{
 		$expected = "./tests/res/psalm/plugins/audio-record";
 		Util::changePsalmProjectDir("./tests/res/psalm/plugins/audio-record", "./tests/res/psalm/psalm.xml");
 		$actual = simplexml_load_file("./tests/res/psalm/psalm.xml")->projectFiles->directory["name"];
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals($expected, $actual);
 	}
-	public function testChangePsalmProjectDirWithoutNameAttribute():void {
+	public function testChangePsalmProjectDirWithoutNameAttribute(): void
+	{
 		$expected = "./tests/res/psalm/plugins/audio-record";
 
 		$xml = simplexml_load_file("./tests/res/psalm/psalm.xml");
@@ -54,12 +61,13 @@ class UtilTest extends TestCase {
 		Util::changePsalmProjectDir("./tests/res/psalm/plugins/audio-record", "./tests/res/psalm/psalm.xml");
 		$actual = simplexml_load_file("./tests/res/psalm/psalm.xml")->projectFiles->directory["name"];
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals($expected, $actual);
 	}
 
 
-	private function rewindXML(): void {
-		$xml = simplexml_load_file( "./tests/res/psalm/psalm.xml" );
+	private function rewindXML(): void
+	{
+		$xml = simplexml_load_file("./tests/res/psalm/psalm.xml");
 
 		$xml->projectFiles = null;
 		$xml->asXML("./tests/res/psalm/psalm.xml");
